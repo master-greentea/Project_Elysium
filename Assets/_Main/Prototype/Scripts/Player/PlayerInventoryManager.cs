989b40d1715d86bc50068833f77cf62459a32115
Steve Hu <yh3133@nyu.edu>
2023-03-07 00:58:46 -0500

    prebuild debug

--- Assets/_Main/Prototype/Scripts/Player/PlayerInventoryManager.cs
+++ Assets/_Main/Prototype/Scripts/Player/PlayerInventoryManager.cs
@@ -1,17 +1,15 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Timeline.Actions;
 using UnityEngine;
 
 public class PlayerInventoryManager : MonoBehaviour
 {
-    public static PlayerInventoryManager Instance;
     private List<ItemBase> itemsList = new List<ItemBase>();
 
     private void Awake()
     {
-        Instance = this;
+        
     }
 
     public void AddItem(ItemBase item)
